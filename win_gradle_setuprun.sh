a44ba76d1a6c09804772b66c6728fd50f04aad65
Chen Wen Kang <cwk1998@hotmail.com>
2019-11-15 06:15:46 +0800

    Fix windows travis build command line issue

--- win_gradle_setuprun.sh
+++ win_gradle_setuprun.sh
@@ -1,10 +1,11 @@
+sh RefreshEnv.sh &&
 choco install gradle -y &&
 echo "openjdk8" &&
 choco install jdk8 -y &&
-source ~/.bashrc &&
+refreshenv &&
 ./gradlew build &&
 echo "openjdk10" &&
 choco uninstall jdk8 -y &&
 choco install zulu10 -y &&
-source ~/.bashrc &&
+refreshenv &&
 ./gradlew build
